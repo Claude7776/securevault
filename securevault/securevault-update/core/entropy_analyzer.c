ba81806677a7d6f388fcf16e2650f4855cc13e06
Claude7776 <codecld@proton.me>
2026-03-07 16:27:05 +0100

    fix: norme42

--- securevault/securevault-update/core/entropy_analyzer.c
+++ securevault/securevault-update/core/entropy_analyzer.c
@@ -2,8 +2,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
-void analyze_entropy(const uint8_t *data, size_t len) {
-    (void)data; // Suppression de l'avertissement de variable non utilisée
-    (void)len; // Suppression de l'avertissement de variable non utilisée
-    printf("[Entropy] Analyse simulée de %zu bytes.\n", len);
+void	analyze_entropy(const uint8_t *data, size_t len)
+{
+	(void)data;
+	(void)len;
+	printf("[Entropy] Analyse simulée de %zu bytes.\n", len);
 }
