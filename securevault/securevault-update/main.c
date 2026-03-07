6db6aad28c7b60d4263429b976429dd9627677e0
Claude7776 <codecld@proton.me>
2026-03-07 16:42:09 +0100

    feat: bibliotheque string and fix norme 42

--- securevault/securevault-update/main.c
+++ securevault/securevault-update/main.c
@@ -1,24 +1,22 @@
 #include <stdio.h>
-#include <stdint.h>
 #include <string.h>
-
 #include "vault/vault.h"
 #include "intelligence/intelligence.h"
 
 int	main(void)
 {
-	int	scrore;
-	uint8_t
+	int	score;
+	uint8_t	
 		ciphertext[128];
-	uint8_t
+	uint8_t	
 		decrypted[64];
-	uint8_t
+	uint8_t	
 		data[64];
-	uint8_t
+	uint8_t	
 		password[32];
 
-	data[64] = "SecureVault test message";
-	password[32] = "superpassword";
+	strcpy((char *)data, "SecureVault test message");
+	strcpy((char *)password, "superpassword");
 	printf("Message original: %s\n", data);
 	vault_encrypt_data(data, ciphertext, password);
 	vault_decrypt_data(ciphertext, decrypted, password);
