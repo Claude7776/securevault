7f8ab3ca875d4466b5f67cb5b3816c3b22a76320
Claude7776 <codecld@proton.me>
2026-03-08 03:53:56 +0100

    fix: fix norme 42

--- securevault/securevault-update/crypto/aes.c
+++ securevault/securevault-update/crypto/aes.c
@@ -2,14 +2,14 @@
 #include <string.h>
 #include <stdio.h>
 
-void aes_encrypt(const uint8_t *plaintext, uint8_t *ciphertext, const uint8_t *key) {
-    // Squelette : copie directe
+void	aes_encrypt(const uint8_t *plaintext, uint8_t *ciphertext, const uint8_t *key)
+{// Squelette : copie directe
     (void)key; // Suppression de l'avertissement de variable non utilisée
     memcpy(ciphertext, plaintext, 64);
     printf("[AES] Encryption simulée.\n");
 }
 
-void aes_decrypt(const uint8_t *ciphertext, uint8_t *plaintext, const uint8_t *key) {
+void	aes_decrypt(const uint8_t *ciphertext, uint8_t *plaintext, const uint8_t *key) {
     (void)key; // Suppression de l'avertissement de variable non utilisée
     memcpy(plaintext, ciphertext, 64);
     printf("[AES] Décryption simulée.\n");
